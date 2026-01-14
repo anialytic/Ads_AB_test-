add28f382441c6e53a82baf3244d7bd2feaf6324
Ania <anna.kors.19.1@gmail.com>
2026-01-14 19:59:35 +0200

    Fix bug + count groups

--- a-b_test.py
+++ a-b_test.py
@@ -1,7 +1,12 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('C:\\Users\\HP\\Desktop\\Ads\\marketing_AB.csv', sep=';')
+df = pd.read_csv('C:\\Users\\HP\\Desktop\\Ads\\marketing_AB.csv', sep=';', low_memory=False)
 
 ad_group_count = df[df['test group']=='ad'].shape[0]
+psa_group_count = df[df['test group']=='psa'].shape[0]
+
+#check rows count
 print(ad_group_count)
+print(psa_group_count)
+print(ad_group_count + psa_group_count)
