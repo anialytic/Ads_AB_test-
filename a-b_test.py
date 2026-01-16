96446e9e4316800e32d47a2ccd6191b5f5c5e2c8
Ania <anna.kors.19.1@gmail.com>
2026-01-16 18:22:00 +0200

    Z-test

--- a-b_test.py
+++ a-b_test.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from statsmodels.stats.proportion import proportions_ztest
 
 df = pd.read_csv('C:\\Users\\HP\\Desktop\\Ads\\marketing_AB.csv', sep=';', low_memory=False)
 
@@ -12,9 +13,24 @@ print(psa_group_count)
 print(ad_group_count + psa_group_count)
 
 #count conversion
-conv_rate = (
-    df.groupby('test group')['converted']
-      .value_counts(normalize=True)
-      .unstack()
-)
+conv_rate = df.groupby('test group')['converted'].value_counts(normalize=True).unstack()
 print(conv_rate)
+print('ok')
+
+#delete unimportant columns
+df = df[df['test group'].isin(['ad', 'psa'])]
+
+# convert boolean to 1/0
+df['converted'] = df['converted'].map({'TRUE': 1, 'FALSE': 0})
+
+#z-test
+summary = df.groupby('test group')['converted'].agg(['sum', 'count'])
+
+#print(summary)
+#print(summary.shape)
+
+conversions = summary['sum'].to_numpy()
+users = summary['count'].to_numpy()
+
+z_stat, p_value = proportions_ztest(conversions, users)
+print(f"Z-stat: {z_stat:.3f}, P-value: {p_value:.5f}")
